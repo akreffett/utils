7e9dc5d7e20f9785b1c24dc607eab8ae9e9f1217
Ivan Kirillov <ikirillov@mitre.org>
2013-01-04 13:50:54 -0500

    Added create_element_from_dict method

--- API/cybox_helper/objects/common_methods.py
+++ API/cybox_helper/objects/common_methods.py
@@ -1,4 +1,27 @@
 
+def create_element_from_dict(element_object, element_dict):
+    for key, value in element_dict.items():
+        if key == 'id': element_object.set_id(value)
+        elif key == 'idref': element_object.set_idref(value)
+        elif key == 'datatype': element_object.set_datatype(value)
+        elif key == 'condition': element_object.set_condition(value)
+        elif key == 'pattern_type': element_object.set_pattern_type(value)
+        elif key == 'regex_syntax': element_object.set_regex_syntax(value)
+        elif key == 'start_range': element_object.set_start_range(value)
+        elif key == 'end_range': element_object.set_end_range(value)
+        elif key == 'value_set': element_object.set_value_set(value)
+        elif key == 'has_changed': element_object.set_has_changed(value)
+        elif key == 'trend': element_object.set_trend(value)
+        elif key == 'appears_random': element_object.set_appears_random(value)
+        elif key == 'is_obfuscated': element_object.set_is_obfuscated(value)
+        elif key == 'obfuscation_algorithm_ref': element_object.set_obfuscation_algorithm_ref(value)
+        elif key == 'is_defanged': element_object.set_is_defanged(value)
+        elif key == 'defanging_algorithm_ref': element_object.set_defanging_algorithm_ref(value)
+        elif key == 'refanging_transform_type': element_object.set_refanging_transform_type(value)
+        elif key == 'refanging_transform': element_object.set_refanging_transform(value)
+        elif key == 'value': element_object.set_valueOf_(value)
+    return element_object
+
 def parse_element_into_dict(element):
     element_dict = {}
     if element.get_id() is not None: element_dict['id'] = element.get_id()
