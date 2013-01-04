637275c7e0324700ad19ec72075c7202b3886862
Ivan Kirillov <ikirillov@mitre.org>
2013-01-04 10:36:13 -0500

    Updated address_object to conform to new interface methodology

--- API/cybox_helper/objects/address_object.py
+++ API/cybox_helper/objects/address_object.py
@@ -1,51 +1,48 @@
-import maec_bundle_3_0 as maecbundle
+import common_methods
+import cybox.cybox_common_types_1_0 as cybox_common
 import cybox.address_object_1_2 as cybox_address_object
 
 class address_object:
-    def __init__(self, id):
-        self.id = id
-        
-    def build_object(self, address_attributes):
-        cybox_object = maecbundle.cybox_core_1_0.AssociatedObjectType(id=self.generator.generate_obj_id(), type_="IP Address")
-        addr_object = self.cybox_address_from_dict(address_attributes)
-        
-        for key, value in address_attributes.items():
-            if key == 'related_objects':
-                related_objects = maecbundle.cybox_core_1_0.RelatedObjectsType()
-                for related_object in value:
-                    related_objects.add_Related_Object(related_object)
-                if related_objects.hasContent_():
-                    cybox_object.set_Related_Objects(related_objects)
-            elif key == 'association' and self.__value_test(value):
-                cybox_object.set_association_type(value)
-        
-        if addr_object.hasContent_():
-            cybox_object.set_Defined_Object(addr_object)
-        
-        return cybox_object
+    def __init__(self):
+        pass
     
     @classmethod
-    def cybox_address_from_dict(self, address_attributes):
+    def create_from_dict(self, address_attributes):
         addrobject = cybox_address_object.AddressObjectType()
         addrobject.set_anyAttributes_({'xsi:type' : 'AddressObj:AddressObjectType'})
         
         for key, value in address_attributes.items():
-            if key == 'category' and self.__value_test(value):
+            if key == 'category' and common_methods.test_value(value):
                 addrobject.set_category(value)
-            elif key == 'ext_category' and self.__value_test(value):
+            elif key == 'ext_category' and common_methods.test_value(value):
                 addrobject.set_Ext_Category(value)
-            elif key == 'vlan_name' and self.__value_test(value):
-                addrobject.set_VLAN_Name(maecbundle.cybox_common_types_1_0.StringObjectAttributeType(datatype='String', valueOf_=maecbundle.quote_xml(value)))
-            elif key == 'vlan_num' and self.__value_test(value):
-                addrobject.set_VLAN_Num(maecbundle.cybox_common_types_1_0.IntegerObjectAttributeType(datatype='Integer', valueOf_=maecbundle.quote_xml(value)))
-            # TODO: implement Is_Source and Is_Destination for AddressObject
-            #elif key == 'is_source':
-            #    if self.__value_test(value):
-            #        addrobject.set_Is_Source(value?true:false)
-            #elif key == 'is_destination':
-            #    if self.__value_test(value):
-            #        addrobject.set_Is_Destination(value?true:false)
-            elif key == 'address_value' and self.__value_test(value):
-                addrobject.set_Address_Value(maecbundle.cybox_common_types_1_0.StringObjectAttributeType(datatype='String', valueOf_=maecbundle.quote_xml(value)))
-            
-        return 
+            elif key == 'vlan_name' and common_methods.test_value(value):
+                addrobject.set_VLAN_Name(cybox_common.StringObjectAttributeType(datatype='String', valueOf_=cybox_common.quote_xml(value)))
+            elif key == 'vlan_num' and common_methods.test_value(value):
+                addrobject.set_VLAN_Num(cybox_common.IntegerObjectAttributeType(datatype='Integer', valueOf_=cybox_common.quote_xml(value)))
+            elif key == 'is_source' and common_methods.test_value(value):
+                addrobject.set_is_source(value)
+            elif key == 'is_destination' and common_methods.test_value(value):
+                addrobject.set_is_destination(value)
+            elif key == 'address_value' and self.test_value(value):
+                addrobject.set_Address_Value(cybox_common.StringObjectAttributeType(datatype='String', valueOf_=cybox_common.quote_xml(value)))
+        return addrobject
+
+    def parse_into_dict(self, defined_object, defined_object_dict = None):
+        if defined_object_dict == None:
+            defined_object_dict = {}
+        if defined_object.get_category() is not None:
+            defined_object_dict['category'] = defined_object.get_category()
+        if defined_object.get_is_source() is not None:
+            defined_object_dict['is_source'] = defined_object.get_is_source()
+        if defined_object.get_is_destination() is not None:
+            defined_object_dict['is_destination'] = defined_object.get_is_destination()
+        if defined_object.get_Address_Value() is not None:
+            defined_object_dict['address_value'] = common_methods.parse_object_attribute_type(defined_object.get_Address_Value())
+        if defined_object.get_Ext_Category() is not None:
+            defined_object_dict['ext_category'] = common_methods.parse_object_attribute_type(defined_object.get_Ext_Category())
+        if defined_object.get_VLAN_Name() is not None:
+            defined_object_dict['vlan_name'] = common_methods.parse_object_attribute_type(defined_object.get_VLAN_Name())
+        if defined_object.get_VLAN_Num() is not None:
+            defined_object_dict['vlan_num'] = common_methods.parse_object_attribute_type(defined_object.get_VLAN_Num())
+        return defined_object_dict
