037bfc1c1d3a2487de14beb58fda9f7546dc69d6
Ivan Kirillov <ikirillov@mitre.org>
2013-01-04 09:46:26 -0500

    Updated port_object to conform with new methodology and interface

---
+++ API/cybox_helper/objects/port_object.py
@@ -0,0 +1,28 @@
+import common_methods
+import cybox.cybox_common_types_1_0 as cybox_common
+import cybox.port_object_1_3 as cybox_port_object
+
+class port_object(object):
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def create_from_dict(cls, port_attributes):
+        portobj = cybox_port_object.PortObjectType()
+        portobj.set_anyAttributes_({'xsi:type' : 'PortObj:PortObjectType'})
+        for key, value in port_attributes.items():
+            if key == 'port_value' and common_methods.test_value(value):
+                portobj.set_Port_Value(cybox_common.PositiveIntegerObjectAttributeType(datatype='PositiveInteger', valueOf_=cybox_common.quote_xml(value)))
+            elif key == 'layer4_protocol' and common_methods.test_value(value):
+                portobj.set_Layer4_Protocol(cybox_common.StringObjectAttributeType(datatype='String', valueOf_=value))
+        return portobj
+
+    @classmethod
+    def parse_into_dict(cls, defined_object, defined_object_dict = None):
+        if defined_object_dict == None:
+            defined_object_dict = {}
+        if defined_object.get_Port_Value() is not None:
+            defined_object_dict['port_value'] = common_methods.parse_object_attribute_type(defined_object.get_Port_Value())
+        if defined_object.get_Layer4_Protocol() is not None:
+            defined_object_dict['layer4_protocol'] = common_methods.parse_object_attribute_type(defined_object.get_Layer4_Protocol())
+        return defined_object_dict
